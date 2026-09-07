f7cdd28ad77bea2130517dbda45640538cfaed3b
Mikael Pettersson <mikael.pettersson@gmail.com>
2026-09-07 11:36:26 +0200

    Exercise nonneutral tone through keyboard save reopen and PNG

--- src/studio/files/tests.rs
+++ src/studio/files/tests.rs
@@ -98,8 +98,17 @@ fn project_open_keyboard_save_reopen_and_export_keep_the_same_pixels() {
     app.request(Action::Open(Some(path.clone()), false), &ctx);
     drain(&mut app, &ctx);
     assert!(app.error.is_none(), "{:?}", app.error);
-    app.editor
-        .edit(|document, _| document.layers[0].exposure = 0.5);
+    app.editor.edit(|document, _| {
+        let layer = &mut document.layers[0];
+        layer.exposure = 0.5;
+        layer.levels = vibeshop::curves::Levels {
+            black: 0.1,
+            gamma: 1.4,
+            white: 0.9,
+        };
+        layer.curves[0].set(16, 0.3).unwrap();
+        layer.curves[2].set(24, 0.8).unwrap();
+    });
     let mut top = app.editor.document.layers[0].clone();
     top.blend = Blend::Multiply;
     top.opacity = 0.4;
